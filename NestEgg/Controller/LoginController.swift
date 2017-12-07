b8b6a777da693fedcfdc448bfd41d6040c3d3916
Matthew Florkiewicz <mflorkie@Matthews-MacBook-Pro-2.local>
2017-12-07 12:54:50 -0500

    Fixed regular login

--- NestEgg/Controller/LoginController.swift
+++ NestEgg/Controller/LoginController.swift
@@ -1,10 +1,21 @@
 import UIKit
 import FBSDKLoginKit
 import GoogleSignIn
+import Alamofire
+import SwiftyJSON
 
 var loggedIn: Bool = false
 
 class LoginController: UIViewController, FBSDKLoginButtonDelegate, GIDSignInUIDelegate {
+    
+    let login_url = "http://nyu-ios-restapi.us-east-2.elasticbeanstalk.com/login/"
+    let checkSession_url = ""
+    
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    
+    var login_session:String = ""
     
     func updateDisplay() {
         
@@ -15,26 +26,120 @@ class LoginController: UIViewController, FBSDKLoginButtonDelegate, GIDSignInUIDe
         
         redirectIfLoggedIn()
         
+        //Google Login Button
         GIDSignIn.sharedInstance().uiDelegate = self
         let googleLoginButton = GIDSignInButton()
         view.addSubview(googleLoginButton)
         googleLoginButton.frame = CGRect(x:16, y: 650, width: view.frame.width - 32, height:50)
         
+        //Facebook Login Button
+        let fbLoginButton = FBSDKLoginButton()
+        view.addSubview(fbLoginButton)
+        fbLoginButton.frame = CGRect(x:16, y:550, width: view.frame.width - 32, height:50)
+        fbLoginButton.delegate = self
         
-        let loginButton = FBSDKLoginButton()
-        view.addSubview(loginButton)
-        
-        loginButton.frame = CGRect(x:16, y:550, width: view.frame.width - 32, height:50)
-        
-        loginButton.delegate = self
-        
+//        //Test Credentials
+//        usernameField.text = "test1"
+//        passwordField.text = "test1"
+
+
+        let preferences = UserDefaults.standard
+        if preferences.object(forKey: "session") != nil
+        {
+            LoginDone()
+        }
+        else
+        {
+            LoginToDo()
+        }
+
         updateDisplay()
     }
+    
+    @IBAction func LoginButton(_ sender: Any) {
+        if(loginButton.titleLabel?.text == "Logout") {
+            let preferences = UserDefaults.standard
+            preferences.removeObject(forKey: "session")
+            LoginToDo()
+            return
+        }
+        
+        let username = usernameField.text
+        let password = passwordField.text
+        
+        if username == "" || password == "" {
+            return
+        }
+        
+        DoLogin(username!, password!)
+    }
+    
+    func DoLogin(_ user: String, _ password: String) {
+        print("got to login")
+        let url_request = "\(login_url)username=\(user)&password=\(password)"
+        print(url_request)
+        Alamofire.request(url_request).responseJSON { response in
+            print(response.result.value)
+            if let result = response.result.value {
+                let json = JSON(result)
+//                print(json["url"])
+//                print(json["explanation"])
+                print(json["success"])
+                if(json["success"] == true) {
+                    print(user)
+                    print(password)
+                    self.LoginDone()
+                }
+            }
+        }
+    }
+    
+    
+    func LoginDone()
+    {
+        usernameField.isEnabled = false
+        passwordField.isEnabled = false
+        
+        loginButton.isEnabled = true
+        
+        print("Login done")
+        loggedIn = true
+        
+        loginButton.setTitle("Logout", for: .normal)
+        redirectIfLoggedIn()
+    }
+    
+    func LoginToDo() {
+        usernameField.isEnabled = true
+        passwordField.isEnabled = true
+        
+        loginButton.isEnabled = true
+        
+        loginButton.setTitle("Login", for: .normal)
+    }
+    
+    
+    
+//    @IBAction func DoLogin(_ sender: AnyObject) {
+//
+//        if(login_button.titleLabel?.text == "Logout")
+//        {
+//            let preferences = UserDefaults.standard
+//            preferences.removeObject(forKey: "session")
+//
+//            LoginToDo()
+//        }
+//        else{
+//            login_now(username:username_input.text!, password: password_input.text!)
+//        }
+//
+//    }
     
     func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
         print("Did log out of facebook")
         redirectIfLoggedIn()
     }
+    
     
     func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!) {
         if error != nil {
--- Podfile
+++ Podfile
@@ -8,5 +8,6 @@ target 'NestEgg' do
   # Pods for NestEgg
 
   pod ‘GoogleSignIn’
+  pod 'AlamofireSwiftyJSON'
 
 end
